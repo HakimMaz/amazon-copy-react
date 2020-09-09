79dc00c63274408c8023f562695247f49e251681
HakimMaz <maz.hakim93@gmail.com>
2020-09-09 13:45:30 +0100

    justifying product component

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Home.css';
+import Product from "./Product";
 
 
 
@@ -11,9 +12,13 @@ function Home() {
               <img src={'https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg'}
                alt="logo" 
                className="home_image"/>
-               <div className="home_row"></div>
-               <div className="home_row"></div>
-               <div className="home_row"></div>
+               <div className="home_row">
+                    <Product/>
+               </div>
+               <div className="home_row">
+               </div>
+               <div className="home_row">
+               </div>
 
 
 
--- src/Product.js
+++ src/Product.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from "react";
 import "./Product.css";
 function Product() {
-    return (
-        <div className="product">
-            
-        </div>
-    )
+  return (
+    <div className="product">
+      <div className="product_info">
+        <p> The lean product</p>
+        <p className="product_price">
+          <small>$</small>
+          <strong>13.08</strong>
+        </p>
+      </div>
+      <div className="product_rating">
+        <p>⭐</p>
+      </div>
+      
+      <img src="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg" alt=""/>
+      
+      <button>Add to Basket</button> 
+    </div>
+  );
 }
 
-export default Product
+export default Product;
