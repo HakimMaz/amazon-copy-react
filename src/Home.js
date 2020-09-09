ace7ea4780252cd5912eb518de013168a13baee4
HakimMaz <maz.hakim93@gmail.com>
2020-09-09 03:00:40 +0100

    adding home component background

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import './Home.css';
+
+
 
 function Home() {
+    
     return (
-        <div>
-            hello home
+        <div className="home">
+            <div className="home_container">
+              <img src={'https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg'}
+               alt="logo" 
+               className="img"/>
+
+            </div>
         </div>
     )
 }
