78c51a100949b6f85c81438ec667c8c7ae0ea98f
HakimMaz <maz.hakim93@gmail.com>
2020-09-09 01:27:07 +0100

    creating navbar

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDdiAcGZN-YQLzv5VFWj86NhAhO3Ln4mfo",
+    authDomain: "challenge-aa993.firebaseapp.com",
+    databaseURL: "https://challenge-aa993.firebaseio.com",
+    projectId: "challenge-aa993",
+    storageBucket: "challenge-aa993.appspot.com",
+    messagingSenderId: "657443661059",
+    appId: "1:657443661059:web:608c4ef8291f282d263536",
+    measurementId: "G-D7VQH2L0EZ"
+  };
