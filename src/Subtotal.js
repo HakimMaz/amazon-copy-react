0e707533107acf8a77175561b60d9f6e7b46d9a2
HakimMaz <maz.hakim93@gmail.com>
2020-09-10 19:21:07 +0100

    adding react-currency-format

---
+++ src/Subtotal.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Subtotal.css"
+
+function Subtotal() {
+    return (
+        <div className="subtotal">
+            here is your subtotal
+        </div>
+    )
+}
+
+export default Subtotal
