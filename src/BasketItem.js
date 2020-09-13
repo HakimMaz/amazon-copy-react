50a5ecddc478dbd290ac449659e350c5f301e09e
HakimMaz <maz.hakim93@gmail.com>
2020-09-13 12:45:02 +0100

    restyling basket item

--- src/BasketItem.js
+++ src/BasketItem.js
@@ -1,40 +1,44 @@
-import React from 'react'
-import './BasketItem.css'
-import { useStateValue } from './StateProvider';
+import React from "react";
+import "./BasketItem.css";
+import { useStateValue } from "./StateProvider";
 
 function BasketItem(props) {
-    const [{basket},dispatch]=useStateValue();
-    console.log("----",props.item.rating);
-    const removeItemFromBasket =(id,price)=>{
-        dispatch({
-            type:"REMOVE_FROM_BASKET",
-            payload:{
-                id:id,
-                price:price,
-            }
-        })       
-    }
-    
-    return (
-        
-        <div className="basketitem">
-          <img src={props.item.image} alt="" className="basketitem_image"/>
-          <div className="basketitem_info">
-             <p>{props.item.title}</p>
-             <p>${props.item.price}</p>
-             <div className="basketitem_rating">
-             {   
-                 Array(props.item.rating)
-                .fill()
-                .map((_,i)=>(
-                <p key={i}>⭐</p>
-                 ))}
-             </div>
-             <button onClick={()=>removeItemFromBasket(props.item.id,props.item.price)}>Remove from Basket</button>
-          </div>
-            
+  const [{ basket }, dispatch] = useStateValue();
+  console.log("----", props.item.rating);
+  const removeItemFromBasket = (id, price) => {
+    dispatch({
+      type: "REMOVE_FROM_BASKET",
+      payload: {
+        id: id,
+        price: price,
+      },
+    });
+  };
+
+  return (
+    <div className="basketitem">
+      <img src={props.item.image} alt="" className="basketitem_image" />
+      <div className="basketitem_info">
+        <p className="basketitem_title">{props.item.title}</p>
+        <p className="basketitem_price">
+            <small>$</small>
+            <strong>{props.item.price}</strong>
+        </p>
+        <div className="basketitem_rating">
+          {Array(props.item.rating)
+            .fill()
+            .map((_, i) => (
+              <p key={i}>⭐</p>
+            ))}
         </div>
-    )
+        <button
+          onClick={() => removeItemFromBasket(props.item.id, props.item.price)}
+        >
+          Remove from Basket
+        </button>
+      </div>
+    </div>
+  );
 }
 
-export default BasketItem
+export default BasketItem;
