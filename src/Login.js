1e62238f4254744b98aabaf0623fc0ec896e9b9f
HakimMaz <maz.hakim93@gmail.com>
2020-09-13 17:21:39 +0100

    styling login component

--- src/Login.js
+++ src/Login.js
@@ -1,10 +1,34 @@
 import React from 'react'
 import './Login.css'
+import { Link } from "react-router-dom";
 
 function Login() {
     return (
         <div className="login">
-            hey login 
+        <Link to="/">
+        <img
+        className="login_logo"
+        src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+         />
+        </Link>
+        <div className="login_container">
+           <h1>Sign-in</h1>
+           <form>
+           <h5>Email</h5>
+           <input type="text"/>
+           <h5>Password</h5>
+           <input type="password"/>
+           <button className="login_signInbutton">Sign-in</button>
+           <p> 
+              By signing-in you agree to Amazon's clone  Conditions of Use
+              & Sale. Please see our Privacy Notice, our coockies Notice
+              and our interest based Ads 
+
+           </p> 
+           <button className="login_registerbutton"> create your Amazon account</button>
+           </form>
+        </div>
+        
         </div> 
     )
 }
