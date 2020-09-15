e21ad70e2ad5fa647391219899a382e5f8679bd2
HakimMaz <maz.hakim93@gmail.com>
2020-09-15 10:56:10 +0100

    inisializing register and signin function

--- src/Login.js
+++ src/Login.js
@@ -1,36 +1,61 @@
-import React from 'react'
-import './Login.css'
+import React, { useState } from "react";
+import "./Login.css";
 import { Link } from "react-router-dom";
 
 function Login() {
-    return (
-        <div className="login">
-        <Link to="/">
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const signIn= e =>{
+      e.preventDefault();
+      console.log("email-------->",email);
+  }
+  const register = e =>{
+      e.preventDefault();
+  }
+  return (
+    <div className="login">
+      <Link to="/">
         <img
-        className="login_logo"
-        src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
-         />
-        </Link>
-        <div className="login_container">
-           <h1>Sign-in</h1>
-           <form>
-           <h5>Email</h5>
-           <input type="text"/>
-           <h5>Password</h5>
-           <input type="password"/>
-           <button className="login_signInbutton">Sign-in</button>
-           <p> 
-              By signing-in you agree to Amazon's clone  Conditions of Use
-              & Sale. Please see our Privacy Notice, our coockies Notice
-              and our interest based Ads 
-
-           </p> 
-           <button className="login_registerbutton"> create your Amazon account</button>
-           </form>
-        </div>
-        
-        </div> 
-    )
+          className="login_logo"
+          src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+        />
+      </Link>
+      <div className="login_container">
+        <h1>Sign-in</h1>
+        <form>
+          <h5>Email</h5>
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <h5>Password</h5>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button 
+          type="submit"
+          className="login_signInbutton"
+          onClick={signIn}
+          >Sign-in</button>
+          <p>
+            By signing-in you agree to Amazon's clone Conditions of Use & Sale.
+            Please see our Privacy Notice, our coockies Notice and our interest
+            based Ads
+          </p>
+          <button
+          type="submit" 
+          className="login_registerbutton"
+          onClick={register}>
+          
+            create your Amazon account
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
